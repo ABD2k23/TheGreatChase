a4a85f9679003569694441e423f3ce904b90f24c
ABD2k23 <136831164+ABD2k23@users.noreply.github.com>
2026-05-06 11:02:14 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-black text-white'>
+      <h1>
+        The Great Chase
+      </h1>
+      <h2>
+        The Great Chase
+      </h2>
+      <h3>
+        The Great Chase
+      </h3>
+    </div>
+  )
+}
+
+export default page
